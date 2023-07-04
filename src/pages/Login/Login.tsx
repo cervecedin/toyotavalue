9bc5af916d55cb2a8bc6d02814a64ae817bc815e
cervecedin <sistemascorasur@gmail.com>
2023-07-04 10:35:28 -0500

    1

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,162 @@
+import * as React from 'react'
+import { useRouter } from 'next/router'
+import { Avatar, Button, CssBaseline, TextField, Box, Container, createTheme, ThemeProvider, Snackbar} from '@mui/material';
+import Axios from 'axios';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import Image from 'next/image'
+import logo from '../../../public/logo.png'
+import { indigo } from '@mui/material/colors';
+import Home from '../index';
+
+const theme = createTheme();
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+
+export default function SignIn(){
+  const router = useRouter()
+  
+  const [Correo_acceso, setCorre_acceso] = React.useState('');
+  const [Contrasena_acceso, setContrasena_acceso] = React.useState('');
+
+    /*SnackBar Alertas Material ui*/
+    const [OpenSnackBar, setOpenSnackBar] = React.useState(false);
+    const [PropSnackbar, setSnackbar] = React.useState({
+      Color: 'error',
+      Mensaje: '',
+    });
+    const { Color, Mensaje } = PropSnackbar;
+    const vertical = 'top';
+    const horizontal = 'left';
+    const handleClose = (event?: React.SyntheticEvent | Event,reason?: string) => {
+      if (reason === "clickaway") {
+        return;
+      }
+      setOpenSnackBar(false);
+    };
+    
+
+  const IniciarSesion = (event:any) => {
+    event.preventDefault();
+    /*const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });*/
+    if (!Correo_acceso || !Contrasena_acceso) {
+      setSnackbar({ Color: 'error', Mensaje: 'Complete Los Datos' });
+      setOpenSnackBar(true);
+    }
+    else {
+      Axios.post('http://192.168.25.141:3000/api/singin', {
+        Correo_acceso: Correo_acceso,
+        Contrasena_acceso: Contrasena_acceso
+      }).then((response) => {
+        if (response.data === "Correo No Registrado") {
+          setSnackbar({ Color: 'error', Mensaje: 'Correo No Registrado' });
+          setOpenSnackBar(true);
+        }
+        else if (response.data === "Acceso Deshabilitado") {
+          setSnackbar({ Color: 'warning', Mensaje: 'Acceso Deshabilitado' });
+          setOpenSnackBar(true);
+        }
+        else if (response.data === "Contraseña Incorrecta") {
+          setSnackbar({ Color: 'error', Mensaje: 'Contraseña Incorrecta' });
+          setOpenSnackBar(true);
+        }
+        else {
+            /*Axios.post('https://192.168.1.244:50000/b1s/v2/Login', {
+                CompanyDB: "SBO_CORASUR",
+                UserName: "manager",
+                Password: "prod123"
+              }, {withCredentials: true}).then((response2) => {                
+                localStorage.setItem("IdServiceLayer", response2.data.SessionId);
+                console.log(response2.data);
+                router.push("/SubModulo");
+              });    */     
+              localStorage.setItem("Acceso", response.data.Id_acceso);
+              localStorage.setItem("Colaborador", response.data.Id_fk_colaborador);
+              localStorage.setItem("Privilegio", response.data.Id_fk_privilegio);
+              localStorage.setItem("Token", response.data.Generatetoken);
+              router.push("/Modulos");
+        }
+
+      });
+    }
+  };
+
+
+  return(
+    <ThemeProvider theme={theme}>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: indigo[700] }} style={{backgroundColor:'#af191b'}}>            
+        </Avatar>
+        <Image src={logo} alt='logo'/>
+        <Box component="form" onSubmit={IniciarSesion} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Addres"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e) => {
+              setCorre_acceso(e.target.value);
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Contraseña"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => {
+              setContrasena_acceso(e.target.value);
+            }}
+          />           
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            style={{backgroundColor:'#af191b'}}
+          >
+            Ingresar
+          </Button>        
+        </Box>
+      </Box>
+
+      {/*Componente Para Alertas*/}
+    <Snackbar anchorOrigin={{ vertical, horizontal }} open={OpenSnackBar} autoHideDuration={2000} onClose={handleClose}>
+      <Alert
+        onClose={handleClose}
+        severity={Color == "success" ? "success" : Color == "error" ? "error" : Color == "info" ? "info" : "warning"}
+        elevation={6}
+        variant="filled"
+      >
+        {Mensaje}
+      </Alert>
+    </Snackbar>
+
+    </Container>
+  </ThemeProvider>
+  )
+}
---
+++ src/pages/Modulos/SideNavBar.tsx
@@ -0,0 +1,155 @@
+import * as React from 'react';
+import Axios from 'axios';
+import { styled, useTheme } from '@mui/material/styles';
+import {Box,Drawer,CssBaseline,Toolbar,List,Typography,Divider,IconButton,ListItem,Icon,ListItemIcon,ListItemText} from '@mui/material';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Link from 'next/link'
+
+const drawerWidth = 240;
+
+interface Props{
+    children: React.ReactNode | React.ReactNode[];
+}
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function SideNavBar({children} : Props) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  /*Estado para mostrar submodulos de un provilegio*/
+  const [Submodulos, setSubmodulos] = React.useState<any[]>([]);
+
+    /*Funcion para mostrar modulos en base a privilegio del localstorage*/
+    const LeerSubmodulo = async () => {
+      await Axios.post('http://192.168.25.141:3000/api/rprivilegios', {
+        Fk_Id_Privilegio: parseInt(localStorage.getItem("Privilegio")!)
+      }, {
+        headers: {
+          'Token': localStorage.getItem("Token"),
+        }
+      }).then((response) => {
+        setSubmodulos(response.data)
+        /*console.log(response.data);*/
+      });
+    }
+     /*Hook para mostar Submodulos al cargar la pagina*/
+    React.useEffect(() => {
+      LeerSubmodulo();
+    }, []);
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Repuestos Lima S.R.L.
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          }
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {Submodulos.map(submodulo => (
+            <Link href={`/${encodeURIComponent(submodulo.Titulo_modulo)}/${encodeURIComponent(submodulo.Titulo_submodulo)}`}  key={submodulo.Id_submodulo} className='my-link'>
+              <ListItem key={submodulo.Id_submodulo} title={submodulo.Titulo_submodulo}>
+                <ListItemIcon><Icon color="info">{submodulo.Icono_submodulo}</Icon></ListItemIcon>
+                <ListItemText primary={submodulo.Descripcion_submodulo} />              
+              </ListItem>
+            </Link>
+          ))}
+        </List>  
+      </Drawer>
+      <Main open={open}>
+      <DrawerHeader />
+        {/*Cargamos Vista de Modulos como prop*/}
+        {children}    
+      </Main>
+    </Box>
+  );
+}
